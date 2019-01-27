ea26300cc23fc659ac30b370cdc4b85a00679dad
Mike Faraponov <mikefaraponov@users.noreply.github.com>
2019-01-27 14:27:55 +0200

    Update json_web_token.rb

--- app/services/json_web_token.rb
+++ app/services/json_web_token.rb
@@ -2,7 +2,7 @@ class JsonWebToken
   SECRET = Rails.application.secrets.secret_key
 
   def self.generate_cookie(payload, cookies = { expires: 24.hours.from_now.utc })
-    cookies.merge(value: encode(payload.merge(exp: cookies[:expires])))
+    cookies.merge value: encode(payload.merge(exp: cookies[:expires]))
   end
   
   def self.encode(payload)
